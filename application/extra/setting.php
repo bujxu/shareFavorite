9c583d412d6b7e1ebbb99ea668946f63d3aba700
bujxu <30498959+bujxu@users.noreply.github.com>
2018-07-21 14:32:40 +0800

    更新appid appsecret

--- application/extra/setting.php
+++ application/extra/setting.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'img_prefix' => 'http://111.231.121.198/shareFavorite/public/images',
+    'img_prefix' => 'https://share.bujxu.com/shareFavorite/public/images',
     'token_expire_in' => 7200
 ];
--- application/extra/wx.php
+++ application/extra/wx.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'app_id' => 'wx870067e4dfa36d32',
-    'app_secret' => 'c84697bfc1e546daa80e3497cc64534b',
+    'app_id' => 'wxb27bd846bfec0588',
+    'app_secret' => '8a5e8ed00ac202e77db7778e9f388f8b',
     'login_url' => 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
 ];
